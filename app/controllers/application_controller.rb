4627cecd4f2d41da1a3651082542b43c39912bf8
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2017-11-15 17:38:45 -0500

    Fix issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,15 +9,15 @@ class ApplicationController < ActionController::Base
       redirect_to '/'
     end
 
-    user = User.find_by!(username: params[:username])
+    user = User.find_by!(username: params['username'])
 
     rescue ActiveRecord::RecordNotFound => e
       logger.warn e.to_s
-      render json: { error: e.to_s }, status: :internal_server_error
+      render json: { error: e.to_s }, status: :internal_server_error and return
     end
 
     if user
-      render json: { err: 'Username Already Registered' }, status: :conflict
+      render json: { err: 'Username Already Registered' }, status: :conflict and return
     end
 
     authy = Authy::API.register_user(
@@ -29,19 +29,19 @@ class ApplicationController < ActionController::Base
     if ! authy.ok?
       error_msg = "Authy didn't handle well the registration"
       logger.warn error_msg
-      render json: { error: error_msg }, status: :service_unavailable
+      render json: { err: error_msg }, status: :internal_server_error and return
     end
 
     new_user = User.create(
       :username params['username'],
       :email params['email'],
-      :authyId authy.id,
+      :authy_id authy.id,
       :password params['password ']
     )
 
     rescue ActiveRecord::NotCreated => e
       logger.warn e.to_s
-      render json: { error: e.to_s }, status: :internal_server_error
+      render json: { error: e.to_s }, status: :internal_server_error and return
     end
 
     session[:user] = user
