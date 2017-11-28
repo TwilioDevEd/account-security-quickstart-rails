54ec718742449e3f0537186f733074e41cf96388
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2017-11-28 15:45:18 -0500

    Fix phone validation

--- app/controllers/api/verify_controller.rb
+++ app/controllers/api/verify_controller.rb
@@ -24,7 +24,19 @@ class Api::VerifyController < ApplicationController
   end
 
   def verify()
-    response = Authy::API.verify(id: user.authy_id, token: params[:token])
+    phone_number = params[:phone_number]
+    country_code = params[:country_code]
+    token = params[:token]
+
+    if !phone_number || !country_code || !token
+      render json: { err: 'Missing fields' }, status: :internal_server_error and return
+    end
+
+    response = Authy::PhoneVerification.check(
+      verification_code: token,
+      country_code: country_code,
+      phone_number: phone_number
+    )
 
     if ! response.ok?
       render json: { err: 'Verify Token Error' }, status: :internal_server_error and return
