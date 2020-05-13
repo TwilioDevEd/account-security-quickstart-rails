4244e39449f5400328811d64c4c9698c469a70f9
Angela Rojas <arojas@stackbuilders.com>
2020-05-13 12:26:44 -0500

    Added tests

--- spec/controllers/twofa_controller_spec.rb
+++ spec/controllers/twofa_controller_spec.rb
@@ -1,5 +1,149 @@
 require 'rails_helper'
 
-RSpec.describe Api::TwofaController, type: :controller do
+class APIRespose
+  def initialize(ok_value)
+    @ok_value = ok_value
+  end
 
+  def ok?
+    @ok_value
+  end
+end
+
+RSpec.describe Api::TwofaController, type: :controller do
+  before(:all) do
+    User.create({username: 'angie', email: 'angie@test.com', authy_id: '123', password: 'pass'})
+  end
+
+  after(:all) do
+    User.delete_all
+  end
+
+  describe 'POST /sms' do
+    context 'when a user exists' do
+      context 'and everything is ok with Authy request_sms' do
+        it'returns 200' do
+          allow(Authy::API).to receive(:request_sms).and_return(APIRespose.new(true))
+          post :sms, session: { username: 'angie' }
+
+          expect(response.status).to eq(200)
+        end
+      end
+
+      context 'and an error ocurrs with Authy request_sms' do
+        it'returns 500' do
+          allow(Authy::API).to receive(:request_sms).and_return(APIRespose.new(false))
+          post :sms, session: { username: 'angie' }
+
+          expect(response.body).to eql("{\"err\":\"Error requesting SMS token\"}")
+          expect(response.status).to eq(500)
+        end
+      end
+    end
+
+    context 'when a user not exists ' do
+      it'returns 500' do
+        post :sms, session: { username: 'not_exists' }
+
+        expect(response.body).to eql("{\"err\":\"Username Not Found\"}")
+        expect(response.status).to eql(500)
+      end
+    end
+  end
+
+  describe 'POST /voice' do
+    context 'when user exists' do
+      context 'and everything is ok with Authy request_phone_call' do
+        it 'returs 200 code' do
+          allow(Authy::API).to receive(:request_phone_call).and_return(APIRespose.new(true))
+          post :voice, session: { username: 'angie' }
+
+          expect(response.status).to eq(200)
+        end
+      end
+
+      context 'and an error ocurrs with Authy request_phone_call' do
+        it'returns 500' do
+          allow(Authy::API).to receive(:request_phone_call).and_return(APIRespose.new(false))
+          post :voice, session: { username: 'angie' }
+
+          expect(response.body).to eql("{\"err\":\"Error requesting Phone call token\"}")
+          expect(response.status).to eq(500)
+        end
+      end
+    end
+
+    context 'when a user not exists ' do
+      it'returns 500' do
+        post :voice, session: { username: 'not_exists' }
+
+        expect(response.body).to eql("{\"err\":\"Username Not Found\"}")
+        expect(response.status).to eql(500)
+      end
+    end
+  end
+
+  describe 'POST /verify' do
+    context 'when user exists' do
+      context 'and everything is ok with Authy verify' do
+        it 'returs 200 code' do
+          allow(Authy::API).to receive(:verify).and_return(APIRespose.new(true))
+          post :verify, session: { username: 'angie' }
+
+          expect(response.status).to eq(200)
+        end
+      end
+
+      context 'and an error ocurrs with Authy verify' do
+        it'returns 500' do
+          allow(Authy::API).to receive(:verify).and_return(APIRespose.new(false))
+          post :verify, session: { username: 'angie' }
+
+          expect(response.body).to eql("{\"err\":\"Verify Token Error\"}")
+          expect(response.status).to eq(500)
+        end
+      end
+    end
+
+    context 'when a user not exists ' do
+      it'returns 500' do
+        post :verify, session: { username: 'not_exists' }
+
+        expect(response.body).to eql("{\"err\":\"Username Not Found\"}")
+        expect(response.status).to eql(500)
+      end
+    end
+  end
+
+  describe 'POST /onetouch' do
+    context 'when user exists' do
+      context 'and everything is ok with Authy send_approval_request' do
+        it 'returs 200 code' do
+          allow(Authy::OneTouch).to receive(:send_approval_request).and_return(APIRespose.new(true))
+          post :onetouch, session: { username: 'angie' }
+
+          expect(response.status).to eq(200)
+        end
+      end
+
+      context 'and an error ocurrs with Authy send_approval_request' do
+        it'returns 500' do
+          allow(Authy::OneTouch).to receive(:send_approval_request).and_return(APIRespose.new(false))
+          post :onetouch, session: { username: 'angie' }
+
+          expect(response.body).to eql("{\"err\":\"Create Push Error\"}")
+          expect(response.status).to eq(500)
+        end
+      end
+    end
+
+    context 'when a user not exists ' do
+      it'returns 500' do
+        post :onetouch, session: { username: 'not_exists' }
+
+        expect(response.body).to eql("{\"err\":\"Username Not Found\"}")
+        expect(response.status).to eql(500)
+      end
+    end
+  end
 end
--- spec/controllers/verify_controller_spec.rb
+++ spec/controllers/verify_controller_spec.rb
@@ -1,9 +1,69 @@
 require 'rails_helper'
 
-RSpec.describe Api::VerifyController, type: :controller do
-  it 'verify a phone number' do
-    post :start, params: { phone_number: '958902747', country_code: '+593', }
+class PhoneVerificationRespose
+  def initialize(ok_value)
+    @ok_value = ok_value
+  end
 
-    expect(response.status).to eq(500)
+  def ok?
+    @ok_value
+  end
+end
+
+RSpec.describe Api::VerifyController, type: :controller do
+  describe 'POST /start' do
+    context 'when all fields are sent' do
+      it 'returns status 200 if everything is correct' do
+        allow(Authy::PhoneVerification).to receive(:start).and_return(PhoneVerificationRespose.new(true))
+        post :start, params: { phone_number: '123456789', country_code: '+593', via: 'sms' }
+
+        expect(response.status).to eq(200)
+      end
+
+      it 'returns status 500 if something is wrong' do
+        allow(Authy::PhoneVerification).to receive(:start).and_return(PhoneVerificationRespose.new(false))
+        post :start, params: { phone_number: '123456789', country_code: '+593', via: 'sms' }
+
+        expect(response.status).to eq(500)
+        expect(response.body).to eql("{\"err\":\"Error delivering code verification\"}")
+      end
+    end
+
+    context 'when a field is missing' do
+      it 'returns status 500 and error message' do
+        post :start, params: { phone_number: '123456789', country_code: '+593' }
+
+        expect(response.status).to eq(500)
+        expect(response.body).to eql("{\"err\":\"Missing fields\"}")
+      end
+    end
+  end
+
+  describe 'POST /verify' do
+    context 'when all fields are sent' do
+      it 'returns status 200 if everything is correct' do
+        allow(Authy::PhoneVerification).to receive(:check).and_return(PhoneVerificationRespose.new(true))
+        post :verify, params: { phone_number: '123456789', country_code: '+593', token: 'my_token' }
+
+        expect(response.status).to eq(200)
+      end
+
+      it 'returns status 500 if something is wrong' do
+        allow(Authy::PhoneVerification).to receive(:check).and_return(PhoneVerificationRespose.new(false))
+        post :verify, params: { phone_number: '123456789', country_code: '+593', token: 'my_token' }
+
+        expect(response.status).to eq(500)
+        expect(response.body).to eql("{\"err\":\"Verify Token Error\"}")
+      end
+    end
+
+    context 'when a field is missing' do
+      it 'returns status 500 and error message' do
+        post :verify, params: { phone_number: '123456789', country_code: '+593' }
+
+        expect(response.status).to eq(500)
+        expect(response.body).to eql("{\"err\":\"Missing fields\"}")
+      end
+    end
   end
 end
